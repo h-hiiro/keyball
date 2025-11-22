c64b055de264187b6c72f70b4bbfef826c6ebf2f
Hiroaki Tanaka <th.under.2305@gmail.com>
2025-11-22 13:39:42 +0900

    scroll snap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -50,7 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   // Auto enable scroll mode when the highest layer is 3
-  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  utint8_t layer = get_highest_layer(state);
+  switch (layer) {
+  case 1:
+    keyball_set_scroll_mode(true);
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL) break;
+  case 2:
+    keyball_set_scroll_mode(true);
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_HORIZONTAL) break;
+  default:
+    keyball_set_scroll_mode(false);
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL) break;
+  }
+
   return state;
 }
 
