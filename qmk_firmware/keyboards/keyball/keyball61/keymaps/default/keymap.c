d58959c1e81289c45020a0d50588811d2744597f
Hiroaki Tanaka <th.under.2305@gmail.com>
2025-11-22 13:43:12 +0900

    semicolon

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -54,13 +54,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   switch (layer) {
   case 1:
     keyball_set_scroll_mode(true);
-    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL) break;
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL);
+    break;
   case 2:
     keyball_set_scroll_mode(true);
-    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_HORIZONTAL) break;
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_HORIZONTAL);
+    break;
   default:
     keyball_set_scroll_mode(false);
-    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL) break;
+    keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL);
+    break;
   }
 
   return state;
